f2721a88fd20ad7a9c7ddabca87244f495a8acd9
gnananav <44158935+gnananav@users.noreply.github.com>
2021-02-07 20:17:57 +0100

    fixed MissingRequiredArgument error

--- mesh_qt.py
+++ mesh_qt.py
@@ -12,7 +12,7 @@ import requests
 import platform
 import sys
 import random, ast
-from discord.ext.commands import CommandNotFound
+from discord.ext.commands import CommandNotFound,MissingRequiredArgument
 
 bot = commands.Bot(command_prefix = '?', help_command=None)
 bottoken = "Nzk4NTY2ODE4ODEzMDUwOTYw.X_25Tg.Fgr9xvAtE0qkJnmHL_dz4gZ3ofw"
@@ -21,12 +21,7 @@ bottoken = "Nzk4NTY2ODE4ODEzMDUwOTYw.X_25Tg.Fgr9xvAtE0qkJnmHL_dz4gZ3ofw"
 async def on_ready():
 	print('Bot is ready.')
 	pass
-	
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, CommandNotFound):
-        return
-    raise error
+
 
 setfootertext = "@ScriptingTools | <?meshhelp>"
 setfooterimage = "https://images-ext-1.discordapp.net/external/atwFnJRaXHB0ebXrVSPjVWDXe5hL2OQ0JBWopjGcVCY/https/images-ext-2.discordapp.net/external/gGrbK8FUkmby_Ao8mmH9dZ4RI1cvfkhpUNBlIB46XQE/https/media.discordapp.net/attachments/460974692073734164/680067025493950474/Wcu7EAAAAASUVORK5CYII.png"
@@ -669,21 +664,18 @@ async def meshcountries(context):
 	embed.set_footer(text=setfootertext, icon_url=setfooterimage)
 	await context.send(embed=embed)
 
-@mesh.error
-async def on_command_error(ctx,error):
-	embed=discord.Embed(title="Command Error", color=setembedcolor)
-	embed.add_field(name="Error", value="Your are missing an argument", inline=True)
-	embed.add_field(name="Command Format", value="?mesh <meshpid>\n?mesh <link>", inline=False)
-	embed.set_footer(text=setfootertext, icon_url=setfooterimage)
-	await ctx.send(embed=embed)
-
-@qt.error
-async def on_command_error(ctx,error):
-	embed=discord.Embed(title="Command Error", color=setembedcolor)
-	embed.add_field(name="Error", value="Your are missing an argument", inline=True)
-	embed.add_field(name="Command Format", value="?qt <store> <PIDs/SKUs>", inline=False)
-	embed.set_footer(text=setfootertext, icon_url=setfooterimage)
-	await ctx.send(embed=embed)
-
+@bot.event
+async def on_command_error(ctx, error):
+	if isinstance(error, CommandNotFound):
+		return
+	elif isinstance(error,MissingRequiredArgument):
+		embed=discord.Embed(title="Command Error", color=setembedcolor)
+		embed.add_field(name="Error", value="Your are missing an argument", inline=True)
+		embed.add_field(name="Command - SKU Scraper", value="?mesh <meshpid>\n?mesh <link>", inline=False)
+		embed.add_field(name="Command - QT", value="?qt <store> <PIDs/SKUs>", inline=False)
+		embed.set_footer(text=setfootertext, icon_url=setfooterimage)
+		await ctx.send(embed=embed)
+		return
+	raise error
 
 bot.run(bottoken)
