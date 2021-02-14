f70e1082f9fb674314608befc27146aa06689eee
gnananav <44158935+gnananav@users.noreply.github.com>
2021-02-14 04:50:52 +0100

    added nikehelp supported countries

--- nikeearlylinks.py
+++ nikeearlylinks.py
@@ -255,6 +255,7 @@ async def nike(context, link):
 async def nikehelp(context):
 	embed=discord.Embed(title="Nike Early Links HELP", color=setembedcolor)
 	embed.add_field(name="Command Format", value="?nike  <full link here>", inline=False)
+	embed.add_field(name="Supported Countries", value=":flag_ch: Switzerand\n:flag_au: Australia\n:flag_ca: Canada\n:", inline=False)
 	embed.set_footer(text=setfootertext, icon_url=setfooterimage)
 	await context.send(embed=embed)
 
